d0a69b6bee259afd13a9450cabd3c56093367f39
Philip Scott <pscott@zeptohost.com>
2017-09-27 11:09:00 -0400

    Replace a noise viz function

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ import fit from 'canvas-fit'
 var rgbaFns = [
   ['sin(1/n(x/256,y/256,t/2))','n(r,x/256,y/256)','sin(r/g)/100','0.6'],
   ['n(x/256,y/256,t)','sin(r*x/64+r*y/64)','1','0.6'],
-  ['n(x/32+y/32, sin(x*y/128)/2, t*10)', 'r', 'r', '0.6']
+  ['1', 'n(x/1024,y/1024,30*n(x/256,y/256,t/4))/2', '0.2', '0.6']
 ]
 var canvas = $('#bg-canvas')
 var fn = rgbaFns[Math.floor(Math.random() * rgbaFns.length)]
